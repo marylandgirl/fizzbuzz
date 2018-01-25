23f8ceb3f0a7b797da41b06aa23e4f42ef92c5da
Kim Levin <levin.kimberly@gmail.com>
2018-01-25 15:19:05 -0500

    Added Comments

--- src/main/java/com/example/fizzbuzz/MainController.java
+++ src/main/java/com/example/fizzbuzz/MainController.java
@@ -3,6 +3,12 @@ package com.example.fizzbuzz;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Java Bootcamp week 1 challenge
+ * 1/25/2018 Kim Levin
+ * This class displays a list of numbers or the strings "Fizz", "Buzz" or "FizzBuzz" based on
+ *      certain criteria
+ */
 @RestController
 public class MainController {
 
@@ -13,7 +19,7 @@ public class MainController {
 
         for ( int i = 1; i < 101; i++)
         {
-            rpt.append((i%3 == 0 && i%5 == 0)?"fizzbuzz<br/>":(i%3 == 0)?"fizz<br/>":(i%5 == 0)?"buzz<br/>":Integer.toString(i) + "<br/>");
+            rpt.append((i%3 == 0 && i%5 == 0)?"FizzBuzz<br/>":(i%3 == 0)?"Fizz<br/>":(i%5 == 0)?"Buzz<br/>":Integer.toString(i) + "<br/>");
         }
         return rpt.toString();
     }
