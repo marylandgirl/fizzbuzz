f03fa1b6ea68331c8aebdb43ac0328d5902da3a5
Kim Levin <levin.kimberly@gmail.com>
2018-01-25 15:00:50 -0500

    Removed unused variables

--- src/main/java/com/example/fizzbuzz/MainController.java
+++ src/main/java/com/example/fizzbuzz/MainController.java
@@ -10,9 +10,7 @@ public class MainController {
     public String printNums(){
 
         StringBuffer rpt = new StringBuffer();
-        String fizz = null;
-        String buzz = null;
-        String fizzBuzz = null;
+
         for ( int i = 1; i < 101; i++)
         {
             rpt.append((i%3 == 0 && i%5 == 0)?"fizzbuzz<br/>":(i%3 == 0)?"fizz<br/>":(i%5 == 0)?"buzz<br/>":Integer.toString(i) + "<br/>");
