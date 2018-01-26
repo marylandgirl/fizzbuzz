9c0563b5108db7fd1301264f8f2252597d890946
Kim Levin <levin.kimberly@gmail.com>
2018-01-26 10:43:24 -0500

    Added Comments

--- src/main/java/com/example/fizzbuzz/MainController.java
+++ src/main/java/com/example/fizzbuzz/MainController.java
@@ -3,6 +3,8 @@ package com.example.fizzbuzz;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Scanner;
+
 /*
  * Java Bootcamp week 1 challenge
  * 1/25/2018 Kim Levin
@@ -22,6 +24,49 @@ public class MainController {
             rpt.append((i%3 == 0 && i%5 == 0)?"FizzBuzz<br/>":(i%3 == 0)?"Fizz<br/>":(i%5 == 0)?"Buzz<br/>":Integer.toString(i) + "<br/>");
         }
         return rpt.toString();
+    }
+
+    @RequestMapping("/bonus")
+    public String printSelectNums(){
+
+        System.out.println("Enter an ending number");
+        Scanner keyboard = new Scanner(System.in);
+        String ent = keyboard.next();
+        boolean parsable = isParsable(ent);
+
+        while ( !parsable && !ent.equalsIgnoreCase("x") ) {
+            System.out.println("Enter an ending number");
+            keyboard = new Scanner(System.in);
+            ent = keyboard.next();
+            parsable = isParsable(ent);
+        }
+
+        StringBuffer rpt = new StringBuffer();
+
+        int num = 0;
+
+        if ( ent.equalsIgnoreCase("x")) {
+            num = 100;
+        } else if (parsable){
+            num = Integer.parseInt(ent);
+        }
+
+        for ( int i = 1; i < num + 1; i++)
+        {
+            rpt.append((i%3 == 0 && i%5 == 0)?"FizzBuzz<br/>":(i%3 == 0)?"Fizz<br/>":(i%5 == 0)?"Buzz<br/>":Integer.toString(i) + "<br/>");
+        }
+        return rpt.toString();
+    }
+
+
+    public static boolean isParsable(String input){
+        boolean parsable = true;
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            parsable = false;
+        }
+        return parsable;
     }
 }
 
